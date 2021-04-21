76c06dbe2062a6ef51eb008789392f20b4aae108
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-21 13:41:50 -0400

    commit tree 1366599c2b3932cad32eb1c863609d7418aee73f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+document.addEventListener('DOMContentLoaded', function(){
+  fetch(imgUrl).then(r => r.json()).then(data => addDogs(data))
+})
+
+const divis = document.getElementById('dog-image-container')
+
+function addDogs(data){
+  let li = document.createElement('li')
+  for(element in data){
+    li.innerHTML = element
+    divis.appendChild(li)
+  }
+}
